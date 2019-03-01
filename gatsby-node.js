2f302665131dd67f4a0034fcc9f863e8922d2466
Jon Gold <hello@designedbygold.com>
2019-02-28 16:36:31 -0800

    Fix build for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,75 +1,75 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/journal-entry.js`)
-  return graphql(
-    `
-      {
-        allMdx(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+//   const blogPost = path.resolve(`./src/templates/journal-entry.js`)
+//   return graphql(
+//     `
+//       {
+//         allMdx(
+//           sort: { fields: [frontmatter___date], order: DESC }
+//           limit: 1000
+//         ) {
+//           edges {
+//             node {
+//               fields {
+//                 slug
+//               }
+//               frontmatter {
+//                 title
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `
+//   ).then(result => {
+//     if (result.errors) {
+//       throw result.errors
+//     }
 
-    // Create blog posts pages.
-    const posts = result.data.allMdx.edges
+//     // Create blog posts pages.
+//     const posts = result.data.allMdx.edges
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+//     posts.forEach((post, index) => {
+//       const previous = index === posts.length - 1 ? null : posts[index + 1].node
+//       const next = index === 0 ? null : posts[index - 1].node
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-  })
-}
+//       createPage({
+//         path: post.node.fields.slug,
+//         component: blogPost,
+//         context: {
+//           slug: post.node.fields.slug,
+//           previous,
+//           next,
+//         },
+//       })
+//     })
+//   })
+// }
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [
-        path.resolve(__dirname, 'src'),
-        'node_modules'
-      ],
-    },
-  })
-}
+// exports.onCreateWebpackConfig = ({ actions }) => {
+//   actions.setWebpackConfig({
+//     resolve: {
+//       modules: [
+//         path.resolve(__dirname, 'src'),
+//         'node_modules'
+//       ],
+//     },
+//   })
+// }
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === 'Mdx') {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+//   if (node.internal.type === 'Mdx') {
+//     const value = createFilePath({ node, getNode })
+//     createNodeField({
+//       name: `slug`,
+//       node,
+//       value,
+//     })
+//   }
+// }
